99addae9ee167eb8d95137bda50e6b966101663a
Ayush Khare <ayushkhare06@gmail.com>
2017-06-17 15:19:00 +0800

    enabled data binding

--- app/src/main/java/sample/com/codingexercise/MainActivity.java
+++ app/src/main/java/sample/com/codingexercise/MainActivity.java
@@ -1,7 +1,11 @@
 package sample.com.codingexercise;
 
-import android.support.v7.app.AppCompatActivity;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import sample.com.codingexercise.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.setListener(this);
     }
+
+    public void onClickAddButton(View view) {
+        //TODO implement add article feature
+    }
+
 }
