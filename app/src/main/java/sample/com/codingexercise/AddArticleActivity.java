6e42421b284593b11e433c5fa1d8a177f40cd021
Ayush Khare <ayushkhare06@gmail.com>
2017-06-17 17:28:37 +0800

    implemented post button for posting new topic

--- app/src/main/java/sample/com/codingexercise/AddArticleActivity.java
+++ app/src/main/java/sample/com/codingexercise/AddArticleActivity.java
@@ -1,10 +1,15 @@
 package sample.com.codingexercise;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.WindowManager;
+
+import sample.com.codingexercise.Model.ArticleModel;
+import sample.com.codingexercise.databinding.ActivityAddArticleBinding;
 
 /**
  * Created by ayush on 17/6/17
@@ -14,11 +19,21 @@ public class AddArticleActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_article);
+        ActivityAddArticleBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_add_article);
+        binding.setAddArticle(this);
+        binding.setArticle(new ArticleModel());
+        setUpActionBar();
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
+    }
+
+    private void setUpActionBar() {
         setTitle(getString(R.string.add_article_title));
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
+    }
+
+    public void onClickPostButton(View view, String content) {
+        //TODO send article content back to main page
     }
 
     @Override
